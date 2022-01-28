8c4733411a349cd157a5bd9e4213c6984f318170
MinJun <68832065+wkdalswns0427@users.noreply.github.com>
2022-01-28 22:37:55 +0900

    Add files via upload

---
+++ vrpn_path_ext/launch/path_launch.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+from ament_index_python.packages import get_package_share_directory
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+
+parameters_file_path = Path(
+    get_package_share_directory('vrpn_path_ext'),
+    'config',
+    'sample.params.yaml',
+)
+
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        launch_ros.actions.Node(
+            package='vrpn_path_ext',
+            node_executable='vrpn_client_node',
+            output='screen',
+            #emulate_tty=False,
+            parameters=[parameters_file_path],
+        ),
+        launch_ros.actions.Node(
+            package='vrpn_path_ext',
+            node_executable='path_extraction',
+            output='screen',
+            #emulate_tty=False,
+            #parameters=[parameters_file_path],
+        ),
+    ])
